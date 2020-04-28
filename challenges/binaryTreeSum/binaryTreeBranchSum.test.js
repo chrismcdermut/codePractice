c85973f7c22987c198733809614c391fec69bd32
Chris McDermut <chrismcdermut@gmail.com>
2020-04-28 13:34:28 -0700

    saving work for binary tree Branch comparison

--- challenges/binaryTreeSum/binaryTreeBranchSum.test.js
+++ challenges/binaryTreeSum/binaryTreeBranchSum.test.js
@@ -1,7 +1,7 @@
 const bTreeBranchSum = require('./binaryTreeBranchSumSolution');
 
 const Tree1 = {
-  tree: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+  tree: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
   leftSum: 49,
   rightSum: 70,
   biggerBranch: 'Right',
@@ -9,7 +9,7 @@ const Tree1 = {
 }
 
 const Tree2 = {
-  tree: [1,3,4,2,3,5,6,7,8,9,5,4,6,7],
+  tree: [0,1,3,4,2,3,5,6,7,8,9,5,4,6,7],
   leftSum: 37,
   rightSum: 32,
   biggerBranch: 'Left',
@@ -17,11 +17,11 @@ const Tree2 = {
 }
 
 const Tree3 = {
-  tree: [4,34,2,67,4,12,1,21,5,45,9.1],
+  tree: [0,4,34,2,67,4,12,1,21,5,45,9],
   leftSum: 175.1,
   rightSum: 15,
   biggerBranch: 'Left',
-  biggerSum: 175.1
+  biggerSum: 175
 }
 
 describe('binaryTree branch sum test-suite', () => {
--- challenges/binaryTreeSum/binaryTreeBranchSumSolution.js
+++ challenges/binaryTreeSum/binaryTreeBranchSumSolution.js
@@ -1,10 +1,66 @@
-function bTreeBranchSum(binaryTree) {
-  let leftSum
-  let rightSum
+function bTreeBranchSum(binaryTreeArray) {
+  if (!binaryTreeArray) return;
+  if (binaryTreeArray.length === 0) return;
+  let leftSum = 0
+  let rightSum = 0
+  let leftBranchIndicies = {
+    1: 1
+  }
+  let rightBranchIndicies = {
+    2: 1
+  }
 
+  //loop through, and add to left/right branches
+  for(let i=0;i<binaryTreeArray.length;i++){
+    //if first node don't add or add to both
+    if(i===0){continue}
 
+    if(leftBranchIndicies[i]){
+      leftSum += binaryTreeArray[i]
+      continue
+    }
+    if(rightBranchIndicies[i]){
+      rightSum += binaryTreeArray[i]
+      continue
+    }
+
+    //check if is left branch -> add to left branch index list, add to left branch Sum
+    if(leftBranchIndicies[i/2-1] || leftBranchIndicies[i/2-2]){
+      leftBranchIndicies[i] = 1
+      leftSum += binaryTreeArray[i]
+    }
+
+    //check if is right branch -> add to right branch index list, add to left right Sum
+    if(rightBranchIndicies[i/2-2] || rightBranchIndicies[i/2-2]){
+      rightBranchIndicies[i] = 1
+      rightSum += binaryTreeArray[i]
+    }
+  }
 
   return leftSum > rightSum ? leftSum : rightSum
 }
 
 module.exports = bTreeBranchSum
+
+///////////WORKING SOLUTION
+// function bTreeBranchSum(binaryTreeArray) {
+//   if (!binaryTreeArray) return;
+//   if (binaryTreeArray.length === 0) return;
+//
+//   //take the array and which node to check
+//   const sum = (arr, idx) => {
+//       //make sure the index is inside of the array
+//       if (idx - 1 < arr.length) {
+//           //in case -1 represents null pointer
+//           // if (arr[idx - 1] === -1) return 0;
+//
+//           //
+//           return arr[idx - 1] + sum(arr, idx * 2) + sum(arr, idx * 2 + 1);
+//       }
+//       return 0;
+//   };
+//
+//   const leftSum = sum(binaryTreeArray, 2);
+//   const rightSum = sum(binaryTreeArray, 3);
+//   return leftSum > rightSum ? 'Left' : 'Right'
+// }
