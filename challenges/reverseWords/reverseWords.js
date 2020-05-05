2c2aba6853c97c9e6ccbe1fa30ff41cc3467d890
Chris McDermut <chrismcdermut@gmail.com>
2020-05-04 22:25:40 -0700

    saving work on reverse words

--- challenges/reverseWords/reverseWords.js
+++ challenges/reverseWords/reverseWords.js
@@ -1,4 +1,22 @@
-function reverseWords(message) {
+///////INTERVIEW CAKE APPROACH///////
+function reversePhrase(message){
+  const reverseMessage = message.reverse()
+  console.log('reverseMessage')
+  console.log(reverseMessage)
+
+
+
+  return reverseMessage
+}
+
+function reverseWord(){
+  
+}
+
+module.exports = reversePhrase
+
+/////////////MY SOLUTION/////////////
+function reverseWords1(message) {
   let finalMessage = []
   let tempWord = []
   // make the new word until space than switch area in the array
@@ -19,38 +37,35 @@ function reverseWords(message) {
   return finalMessage
 }
 
-module.exports = reverseWords
-
-
 /////////////NODE SCRIPT/////////////
 
-// module.exports.reverseWords = function() {
-//   const message = [ 'c', 'a', 'k', 'e', ' ',
-//             'p', 'o', 'u', 'n', 'd', ' ',
-//             's', 't', 'e', 'a', 'l' ]
-//   console.log('message')
-//   console.log(message)
-//   let finalMessage = []
-//   let tempWord = []
-//   // make the new word until space than switch area in the array
-//   for(let i=0;i<message.length;i++){
-//     if(i===(message.length-1)){
-//         tempWord.push(message[i])
-//         finalMessage.unshift(...tempWord)
-//         continue
-//     }
-//     if(message[i]!==' '){
-//         tempWord.push(message[i])
-//         continue
-//     }
-//     finalMessage.unshift(...tempWord)
-//     finalMessage.unshift(' ')
-//     tempWord = []
-//     //make a word until space
-//     //put word in first place of array
-//     //put space in
-//   }
-//   console.log('finalMessage')
-//   console.log(finalMessage)
-//   return finalMessage
-// }
+module.exports.reverseWords2 = function() {
+  const message = [ 'c', 'a', 'k', 'e', ' ',
+            'p', 'o', 'u', 'n', 'd', ' ',
+            's', 't', 'e', 'a', 'l' ]
+  console.log('message')
+  console.log(message)
+  let finalMessage = []
+  let tempWord = []
+  // make the new word until space than switch area in the array
+  for(let i=0;i<message.length;i++){
+    if(i===(message.length-1)){
+        tempWord.push(message[i])
+        finalMessage.unshift(...tempWord)
+        continue
+    }
+    if(message[i]!==' '){
+        tempWord.push(message[i])
+        continue
+    }
+    finalMessage.unshift(...tempWord)
+    finalMessage.unshift(' ')
+    tempWord = []
+    //make a word until space
+    //put word in first place of array
+    //put space in
+  }
+  console.log('finalMessage')
+  console.log(finalMessage)
+  return finalMessage
+}
--- challenges/reverseWords/reverseWords.test.js
+++ challenges/reverseWords/reverseWords.test.js
@@ -9,11 +9,25 @@ const testOne = {
             'c', 'a', 'k', 'e', ]
 }
 
+const testTwo = {
+  input: [ 'l', 'a', 'n', 'd', 'e', 'd', ' ', 'h', 'a', 's', ' ',
+    'e', 'a', 'g', 'l', 'e', ' ', 't', 'h', 'e' ],
+  output: [ 't', 'h', 'e', ' ', 'e', 'a', 'g', 'l', 'e', ' ', 'h',
+    'a', 's', ' ', 'l', 'a', 'n', 'd', 'e', 'd' ]
+}
+
+
+
 describe('reverseWords Test', () => {
 
   test('testOne', ()=>{
     let result = reverseWords(testOne.input)
     expect(result).toEqual(testOne.output);
+  })
+
+  test('testTwo', ()=>{
+    let result = reverseWords(testTwo.input)
+    expect(result).toEqual(testTwo.output);
   })
 
 })
