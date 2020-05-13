c0505c85c9637e679448e9cf42e467619eb813bd
Chris McDermut <chrismcdermut@gmail.com>
2020-05-12 21:03:39 -0700

    two more files

--- challenges/productOfAll/productOfAll.js
+++ challenges/productOfAll/productOfAll.js
@@ -1,13 +1,10 @@
 function productOfAll(input) {
-  let productResults = []
-  let productBefore = 1
-  let productAfter = 1
-  for(let i=0;i<input.length;i++){
-
-
-
-    productResults.push(productBefore*productAfter)
+  const productResults = [];
+  const productBefore = 1;
+  const productAfter = 1;
+  for (let i = 0; i < input.length; i++) {
+    productResults.push(productBefore * productAfter);
   }
 }
 
-module.exports = productOfAll
+module.exports = productOfAll;
--- challenges/productOfAll/productOfAll.test.js
+++ challenges/productOfAll/productOfAll.test.js
@@ -1,25 +1,23 @@
-const productOfAll = require('./productOfAll')
+const productOfAll = require('./productOfAll');
 
 const testOne = {
   input: [1, 2, 6, 5, 9],
-  output: [540, 270, 90, 108, 60]
-}
+  output: [540, 270, 90, 108, 60],
+};
 
 const testTwo = {
   input: [1, 7, 3, 4],
-  output: [84, 12, 28, 21]
-}
+  output: [84, 12, 28, 21],
+};
 
 describe('productOfAll Test', () => {
-
-  test('testOne', ()=>{
-    let result = productOfAll(testOne.input)
+  test('testOne', () => {
+    const result = productOfAll(testOne.input);
     expect(result).toEqual(testOne.output);
-  })
+  });
 
-  test('testTwo', ()=>{
-    let result = productOfAll(testTwo.input)
+  test('testTwo', () => {
+    const result = productOfAll(testTwo.input);
     expect(result).toEqual(testTwo.output);
-  })
-
-})
+  });
+});
