e07bc5b8f6769de898764c89238ffb6d9cd121e0
Chris McDermut <chrismcdermut@gmail.com>
2020-05-15 16:05:32 -0700

    more work

--- challenges/duplicateElement/duplicateElement.js
+++ challenges/duplicateElement/duplicateElement.js
@@ -1,11 +1,11 @@
-function duplicateElement(input) {
+function duplicateElementCheck(input) {
   let duplicateElement;
-  //sort array
-  //divide in two, if the last element is less then firstelement plus n the duplicate is in there.
-  for(let i=0;i<input.length;i++){
-
+  // sort array
+  // divide in two, if the last element is less then firstelement plus n the duplicate is in there.
+  for (let i = 0; i < input.length; i++) {
+    console.log(i);
   }
   return duplicateElement;
 }
 
-module.exports = duplicateElement
+module.exports = duplicateElementCheck;
--- challenges/duplicateElement/duplicateElement.test.js
+++ challenges/duplicateElement/duplicateElement.test.js
@@ -1,25 +1,23 @@
-const duplicateElement = require('./duplicateElement')
+const duplicateElementCheck = require('./duplicateElement');
 
 const testOne = {
-  input: [1,2,3,4,5,6,7,8,1,9,10],
-  output: 1
-}
+  input: [1, 2, 3, 4, 5, 6, 7, 8, 1, 9, 10],
+  output: 1,
+};
 
 const testOneA = {
-  input: [1,2,3,4,5,6,7,8,9,9,10],
-  output: 1
-}
+  input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10],
+  output: 1,
+};
 
-describe('duplicateElement Test', () => {
+describe('duplicateElementCheck Test', () => {
+  test('testOne', () => {
+    const result = duplicateElementCheck(testOne.input);
+    expect(result).toEqual(testOne.output);
+  });
 
-  test('testOneA', ()=>{
-    let result = duplicateElement(testOneA.input)
+  test('testOneA', () => {
+    const result = duplicateElementCheck(testOneA.input);
     expect(result).toEqual(testOneA.output);
-  })
-
-  test('testOneA', ()=>{
-    let result = duplicateElement(testOneA.input)
-    expect(result).toEqual(testOneA.output);
-  })
-
-})
+  });
+});
