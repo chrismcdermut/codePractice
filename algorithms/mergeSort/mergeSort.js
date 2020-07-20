43e501690c5ef70b6e62398f068b2e764fd5d012
Chris McDermut <chrismcdermut@gmail.com>
2020-07-20 13:17:10 -0700

    clean mergeSort

--- algorithms/mergeSort/mergeSort.js
+++ algorithms/mergeSort/mergeSort.js
@@ -30,3 +30,8 @@ function mergeSort(listArg) {
 }
 
 module.exports = mergeSort;
+
+module.exports.runMergeSort = function () {
+  const exampleList = [4, 56, 1, 14, 11, 67, 23, 3, 7, 89, 16, 2, 91, 34];
+  console.log(mergeSort(exampleList));
+};
