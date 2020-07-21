8cd12caf241f80f4b32095c2c1b1b5c521d8c5bc
Chris McDermut <chrismcdermut@gmail.com>
2020-07-21 15:23:43 -0700

    graphColoring work i think is done

--- challenges/graphColoring/graphColoring.js
+++ challenges/graphColoring/graphColoring.js
@@ -1,6 +1,44 @@
-function graphColoring(input) {
-  console.log('input');
-  console.log(input);
+function colorGraph(graph, colors) {
+  graph.forEach((nodeInput) => {
+    const node = nodeInput;
+    if (node.neighbors.has(node)) {
+      throw new Error(`Legal coloring impossible for node with loop: ${node.label}`);
+    }
+
+    // Get the node's neighbors' colors, as a set so we
+    // can check if a color is illegal in constant time
+    const illegalColors = new Set();
+
+    node.neighbors.forEach((neighbor) => {
+      if (neighbor.color !== null) {
+        illegalColors.add(neighbor.color);
+      }
+    });
+
+    // Assign the first legal color
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i];
+
+      if (!illegalColors.has(color)) {
+        node.color = color;
+        break;
+      }
+    }
+  });
 }
 
-module.exports = graphColoring;
+function checkHasIllegalColor(graph) {
+  let hasIllegalColor = false;
+  graph.forEach((node) => {
+    node.neighbors.forEach((neighbor) => {
+      if (node.color === neighbor.color) {
+        hasIllegalColor = true;
+        return hasIllegalColor;
+      }
+      return null;
+    });
+  });
+  return hasIllegalColor;
+}
+
+module.exports = { colorGraph, checkHasIllegalColor };
--- challenges/graphColoring/graphColoring.test.js
+++ challenges/graphColoring/graphColoring.test.js
@@ -1,13 +1,69 @@
-const graphColoring = require('./graphColoring');
+const GraphNode = require('../../dataStructures/graphNode/graphNode');
+const { colorGraph, checkHasIllegalColor } = require('./graphColoring');
 
-const testOne = {
-  input: '',
-  output: '',
-};
+describe('checkHasIllegalColor Test', () => {
+  // the test should make sure there are no illegal colors..
+  test('testOne, no illegal color', () => {
+    const a = new GraphNode('a');
+    a.color = 'red';
+    const b = new GraphNode('b');
+    b.color = 'green';
+    const c = new GraphNode('c');
+    c.color = 'purple';
 
-xdescribe('graphColoring Test', () => {
-  test('testOne', () => {
-    const result = graphColoring(testOne.input);
-    expect(result).toEqual(testOne.output);
+    a.neighbors.add(b);
+    b.neighbors.add(a);
+    c.neighbors.add(b);
+    b.neighbors.add(c);
+
+    const coloredGraph = [a, b, c];
+    // const coloredGraph = colorGraph(graph, colors);
+    const result = checkHasIllegalColor(coloredGraph);
+    const answer = false;
+    expect(result).toEqual(answer);
+  });
+
+  test('testTwo, has illegal color', () => {
+    const a = new GraphNode('a');
+    a.color = 'red';
+    const b = new GraphNode('b');
+    b.color = 'red';
+    const c = new GraphNode('c');
+    c.color = 'purple';
+
+    a.neighbors.add(b);
+    b.neighbors.add(a);
+    c.neighbors.add(b);
+    b.neighbors.add(c);
+    const coloredGraph = [a, b, c];
+
+    // const coloredGraph = colorGraph(graph, colors);
+    const result = checkHasIllegalColor(coloredGraph);
+    const answer = true;
+    expect(result).toEqual(answer);
+  });
+});
+
+xdescribe('colorGraph Test', () => {
+  // the test should make sure there are no illegal colors..
+
+  test('testOne, no illegal color', () => {
+    const a = new GraphNode('a');
+    const b = new GraphNode('b');
+    const c = new GraphNode('c');
+
+    a.neighbors.add(b);
+    b.neighbors.add(a);
+    c.neighbors.add(b);
+    b.neighbors.add(c);
+    // D is 2
+    const graph = [a, b, c];
+
+    const colors = ['red', 'blue', 'green'];
+
+    const coloredGraph = colorGraph(graph, colors);
+    const result = checkHasIllegalColor(coloredGraph);
+    const answer = false;
+    expect(result).toEqual(answer);
   });
 });
