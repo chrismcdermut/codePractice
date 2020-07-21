43fa5fc1599d392b5cbd4a3f704a8242831d1aea
Chris McDermut <chrismcdermut@gmail.com>
2020-07-20 22:46:37 -0700

    add optimized option

--- challenges/secondLargestBTreeItem/secondLargestBTreeItem.test.js
+++ challenges/secondLargestBTreeItem/secondLargestBTreeItem.test.js
@@ -1,7 +1,7 @@
 const BinaryTreeNode = require('../../dataStructures/binaryTree/BinaryTreeNode');
-const findSecondLargestItem = require('./secondLargestBTreeItem');
+const { findSecondLargest, findSecondLargest2 } = require('./secondLargestBTreeItem');
 
-describe('findSecondLargestItem Test', () => {
+describe('findSecondLargest Test', () => {
   const layer2right3Node = new BinaryTreeNode(6);
   layer2right3Node.insertLeft(4);
   layer2right3Node.insertRight(7);
@@ -21,7 +21,32 @@ describe('findSecondLargestItem Test', () => {
   const answer = 10;
 
   test('testOne', () => {
-    const result = findSecondLargestItem(rootNode);
+    const result = findSecondLargest(rootNode);
+    expect(result).toEqual(answer);
+  });
+});
+
+describe('findSecondLargest2 Test', () => {
+  const layer2right3Node = new BinaryTreeNode(6);
+  layer2right3Node.insertLeft(4);
+  layer2right3Node.insertRight(7);
+
+  const layer1left2Node = new BinaryTreeNode(3);
+  layer1left2Node.insertLeft(1);
+  layer1left2Node.insertRight(layer2right3Node);
+  layer1left2Node.right = layer2right3Node;
+
+  const layer1right2Node = new BinaryTreeNode(10);
+  layer1right2Node.insertRight(14);
+
+  const rootNode = new BinaryTreeNode(8);
+  rootNode.insertLeft(layer1left2Node);
+  rootNode.insertRight(layer1right2Node);
+
+  const answer = 10;
+
+  test('testOne', () => {
+    const result = findSecondLargest2(rootNode);
     expect(result).toEqual(answer);
   });
 });
