ba3c92e841515faa07c61dfa54656d0029dc77f4
Chris McDermut <chrismcdermut@gmail.com>
2020-07-21 20:23:33 -0700

    cleaning up GraphObject punting to another time

--- dataStructures/GraphObject/GraphObject.test.js
+++ dataStructures/GraphObject/GraphObject.test.js
@@ -1,13 +1,19 @@
-const GraphObject = require('./GraphObject');
+// const GraphObject = require('./GraphObject');
 
-const testOne = {
-  input: '',
-  output: '',
-};
-
+// TODO: what is a good test for this?
 xdescribe('GraphObject Test', () => {
-  test('testOne', () => {
-    const result = new GraphObject(testOne.input);
-    expect(result).toEqual(testOne.output);
+  const network = {
+    Min: ['William', 'Jayden', 'Omar'],
+    William: ['Min', 'Noam'],
+    Jayden: ['Min', 'Amelia', 'Ren', 'Noam'],
+    Ren: ['Jayden', 'Omar'],
+    Amelia: ['Jayden', 'Adam', 'Miguel'],
+    Adam: ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
+    Miguel: ['Amelia', 'Adam', 'Liam', 'Nathan'],
+    Noam: ['Nathan', 'Jayden', 'William'],
+    Omar: ['Ren', 'Min', 'Scott'],
+  };
+  xtest('testOne', () => {
+    expect(network).toEqual('something');
   });
 });
