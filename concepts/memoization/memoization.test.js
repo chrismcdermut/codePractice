e729674aa7c90bfa8c3fae8fbe49cfc9c0125c35
Chris McDermut <chrismcdermut@gmail.com>
2020-07-23 17:39:04 -0700

    adding error handling testing

--- concepts/memoization/memoization.test.js
+++ concepts/memoization/memoization.test.js
@@ -1,14 +1,25 @@
 const Memoization = require('./memoization');
 
-const testOne = {
-  input: 5,
-  output: 5,
-};
-
 describe('memoization Test', () => {
   const newFibCounter = new Memoization();
+
   test('testOne', () => {
+    const testOne = {
+      input: 5,
+      output: 5,
+    };
+
     const result = newFibCounter.fib(testOne.input);
     expect(result).toEqual(testOne.output);
+  });
+
+  test('testTwo', () => {
+    const testTwo = {
+      input: -1,
+    };
+
+    expect(() => {
+      newFibCounter.fib(testTwo.input);
+    }).toThrow('Index was negative. No such thing as a negative index in a series.');
   });
 });
