b3706e836411ef06ce4af743108c28e0c7a71c8d
Chris McDermut <chrismcdermut@gmail.com>
2020-07-27 13:59:04 -0700

    sandbox stoof

--- challenges/sandBox/sandBox.js
+++ challenges/sandBox/sandBox.js
@@ -1,19 +1,5 @@
-// const util = require('util');
-
-// console.log(util.inspect(myObject, {showHidden: false, depth: null}))
-
-// alternative shortcut
-// console.log(util.inspect(myObject, false, null, true /* enable colors */))
-
-
 function sandBox(input) {
-  const nodes = [];
-  const treeRoot = 'test';
-  nodes.push([treeRoot, 0]);
-  // console.log('nodes');
-  // console.log(nodes);
-  // console.log(util.inspect(nodes, { showHidden: false, depth: null }));
-  // console.log(util.inspect(nodes, false, null, true /* enable colors */));
+  console.log(input);
   return input;
 }
 
--- challenges/sandBox/sandBox.test.js
+++ challenges/sandBox/sandBox.test.js
@@ -1,12 +1,18 @@
 const sandBox = require('./sandBox');
 
 const testOne = {
-  input: 'test',
-  output: 'test',
+  input: 'Hello world',
+  output: 'Hello world',
 };
 
-xdescribe('sandBox Test', () => {
+describe('sandBox Test', () => {
   test('testOne', () => {
+    console.log = jest.fn();
+    sandBox(testOne.input);
+    expect(console.log).toHaveBeenCalledWith(testOne.output);
+  });
+
+  test('testTwo', () => {
     const result = sandBox(testOne.input);
     expect(result).toEqual(testOne.output);
   });
