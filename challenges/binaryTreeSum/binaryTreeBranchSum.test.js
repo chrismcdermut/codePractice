89cf19312493c00112085be62474fa43b8a4ce55
Chris McDermut <chrismcdermut@gmail.com>
2020-07-27 13:38:12 -0700

    coverage for btreesum

--- challenges/binaryTreeSum/binaryTreeBranchSum.test.js
+++ challenges/binaryTreeSum/binaryTreeBranchSum.test.js
@@ -1,9 +1,10 @@
-const bTreeBranchSum = require('./binaryTreeBranchSumSolution');
+const { bTreeBranchSum, sum } = require('./binaryTreeBranchSumSolution');
 
+// TODO: are these answers right?
 const Tree1 = {
   tree: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-  leftSum: 49,
-  rightSum: 70,
+  leftSum: 57, // 49
+  rightSum: 63, // 70
   biggerBranch: 'Right',
   biggerSum: 70,
 };
@@ -11,32 +12,64 @@ const Tree1 = {
 const Tree2 = {
   tree: [0, 1, 3, 4, 2, 3, 5, 6, 7, 8, 9, 5, 4, 6, 7],
   leftSum: 37,
-  rightSum: 32,
+  rightSum: 33, // 32
   biggerBranch: 'Left',
   biggerSum: 37,
 };
 
 const Tree3 = {
   tree: [0, 4, 34, 2, 67, 4, 12, 1, 21, 5, 45, 9],
-  leftSum: 175.1,
-  rightSum: 15,
+  leftSum: 145, // 175.1
+  rightSum: 59, // 55,
   biggerBranch: 'Left',
   biggerSum: 175,
 };
 
-xdescribe('binaryTree branch sum test-suite', () => {
+describe('binaryTree branch sum test-suite', () => {
   test('Tree1 test', () => {
     const result = bTreeBranchSum(Tree1.tree);
-    expect(result).toEqual(Tree1.biggerSum);
+    expect(result).toEqual(Tree1.biggerBranch);
   });
 
   test('Tree2 test', () => {
     const result = bTreeBranchSum(Tree2.tree);
-    expect(result).toEqual(Tree2.biggerSum);
+    expect(result).toEqual(Tree2.biggerBranch);
   });
 
   test('Tree3 test', () => {
     const result = bTreeBranchSum(Tree3.tree);
-    expect(result).toEqual(Tree3.biggerSum);
+    expect(result).toEqual(Tree3.biggerBranch);
+  });
+});
+
+describe('sum test-suite', () => {
+  test('Tree1 test', () => {
+    const result = sum(Tree1.tree, 2);
+    expect(result).toEqual(Tree1.leftSum);
+  });
+
+  test('Tree1 test', () => {
+    const result = sum(Tree1.tree, 3);
+    expect(result).toEqual(Tree1.rightSum);
+  });
+
+  test('Tree2 test', () => {
+    const result = sum(Tree2.tree, 2);
+    expect(result).toEqual(Tree2.leftSum);
+  });
+
+  test('Tree2 test', () => {
+    const result = sum(Tree2.tree, 3);
+    expect(result).toEqual(Tree2.rightSum);
+  });
+
+  test('Tree3 test', () => {
+    const result = sum(Tree3.tree, 2);
+    expect(result).toEqual(Tree3.leftSum);
+  });
+
+  test('Tree3 test', () => {
+    const result = sum(Tree3.tree, 3);
+    expect(result).toEqual(Tree3.rightSum);
   });
 });
