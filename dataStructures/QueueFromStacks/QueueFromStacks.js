391840e8a552a377201b0142b75eb1fbd05af659
Chris McDermut <chrismcdermut@gmail.com>
2020-08-04 10:24:58 -0700

    midway working

--- dataStructures/QueueFromStacks/QueueFromStacks.js
+++ dataStructures/QueueFromStacks/QueueFromStacks.js
@@ -8,8 +8,6 @@ class QueueFromStacks {
 
   enqueue(item) {
     this.enqueueStack.push(item);
-    console.log('enqueue this.enqueueStack');
-    console.log(this.enqueueStack);
   }
 
   dequeue() {
@@ -18,34 +16,14 @@ class QueueFromStacks {
     }
     // if dequeue stack is empthy, fill it up
     if (this.dequeueStack.items.length === 0) {
-      console.log('this.enqueueStack.items.length');
-      console.log(this.enqueueStack.items.length);
-
       this.enqueueStack.items.forEach((item) => {
         this.dequeueStack.push(item);
       });
 
-      // const enqueueStackLength = this.enqueueStack.items.length;
 
-      // for (let i = 0; i < this.enqueueStack.items.length; i++) {
-      //   console.log('dequeue this.dequeueStack BEFORE');
-      //   console.log(this.dequeueStack);
-      //   console.log('dequeue this.enqueueStack BEFORE');
-      //   console.log(this.enqueueStack);
-      //   this.dequeueStack.push(this.enqueueStack.pop());
-      //   console.log('dequeue this.dequeueStack AFTER');
-      //   console.log(this.dequeueStack);
-      //   console.log('dequeue this.enqueueStack AFTER');
-      //   console.log(this.enqueueStack);
-      // }
-      // console.log('dequeue this.dequeueStack');
-      // console.log(this.dequeueStack);
-      // return this.dequeueStack.pop();
       this.enqueueStack = new Stack();
     }
     if (this.dequeueStack.items.length > 0) {
-      console.log('this.dequeueStack');
-      console.log(this.dequeueStack);
       return this.dequeueStack.pop();
     }
     return null;
--- dataStructures/QueueFromStacks/QueueFromStacksNotes.md
+++
@@ -1,1 +0,0 @@
-QueueFromStacks Notes go here!
