8ea706d490e0990f3a54bf1bce28d15464a5ad51
Chris McDermut <chrismcdermut@gmail.com>
2020-08-06 12:15:50 -0700

    got single linkedlist node working

--- helpers/generateBoilerPlate.js
+++ helpers/generateBoilerPlate.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 // TODO: make this take arguments
 // ////SETUP HERE//////
-const solutionName = 'closerValidator';
+const solutionName = 'LinkedListNodeDouble';
 // TODO: look into setting up map or enum for this
 /* challenge || dataStructure || algorithm || designPattern || concept */
-const codeChallengeType = 'challenge';
+const codeChallengeType = 'dataStructure';
 
 // ////UNIFORM SPEC/NOTES BOILERPLATE//////
 const spec = `${solutionName} Spec goes here!`;
