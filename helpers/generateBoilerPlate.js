ec47a4af330b24c39c0f72104ac6331d1e9b5fe0
Chris McDermut <chrismcdermut@gmail.com>
2020-08-11 11:31:36 -0700

    findRepeat

--- helpers/generateBoilerPlate.js
+++ helpers/generateBoilerPlate.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // TODO: make this take arguments
 // ////SETUP HERE//////
-const solutionName = 'findKthPriorElement';
+const solutionName = 'findDuplicateIn1Space';
 // TODO: look into setting up map or enum for this
 /* challenge || dataStructure || algorithm || designPattern || concept */
 const codeChallengeType = 'challenge';
