5df5640478a03d7bd4a37afc754f844193214fe8
Chris McDermut <chrismcdermut@gmail.com>
2020-08-11 18:43:54 -0700

    carryOperations

--- challenges/determineCarryOperations/determineCarryOperations.test.js
+++ challenges/determineCarryOperations/determineCarryOperations.test.js
@@ -1,4 +1,4 @@
-const { determineCarryOperations } = require('./determineCarryOperations');
+const { findOrderOfMagnitude, convert, determineCarryOperations } = require('./determineCarryOperations');
 
 describe('determineCarryOperations Test', () => {
   test('testOne', () => {
@@ -54,5 +54,99 @@ describe('determineCarryOperations Test', () => {
     };
     const result = determineCarryOperations(testSix.input[0], testSix.input[1]);
     expect(result).toEqual(testSix.output);
+  });
+});
+
+describe('findOrderOfMagnitude', () => {
+  test('testOne', () => {
+    const testOne = {
+      input: 1,
+      output: 0,
+    };
+    const result = findOrderOfMagnitude(testOne.input);
+    expect(result).toEqual(testOne.output);
+  });
+
+  test('testTwo', () => {
+    const testTwo = {
+      input: 16,
+      output: 1,
+    };
+    const result = findOrderOfMagnitude(testTwo.input);
+    expect(result).toEqual(testTwo.output);
+  });
+
+  test('testThree', () => {
+    const testThree = {
+      input: 788,
+      output: 2,
+    };
+    const result = findOrderOfMagnitude(testThree.input);
+    expect(result).toEqual(testThree.output);
+  });
+
+  test('testFour', () => {
+    const testFour = {
+      input: 7896,
+      output: 3,
+    };
+    const result = findOrderOfMagnitude(testFour.input);
+    expect(result).toEqual(testFour.output);
+  });
+
+  test('testFive', () => {
+    const testFive = {
+      input: 14000000000,
+      output: 10,
+    };
+    const result = findOrderOfMagnitude(testFive.input);
+    expect(result).toEqual(testFive.output);
+  });
+});
+
+describe('convert', () => {
+  test('testOne', () => {
+    const testOne = {
+      input: 1,
+      output: 1,
+    };
+    const result = convert(testOne.input);
+    expect(result).toEqual(testOne.output);
+  });
+
+  test('testTwo', () => {
+    const testTwo = {
+      input: 16,
+      output: 10,
+    };
+    const result = convert(testTwo.input);
+    expect(result).toEqual(testTwo.output);
+  });
+
+  test('testThree', () => {
+    const testThree = {
+      input: 788,
+      output: 100,
+    };
+    const result = convert(testThree.input);
+    expect(result).toEqual(testThree.output);
+  });
+
+  test('testFour', () => {
+    const testFour = {
+      input: 7896,
+      output: 1000,
+    };
+    const result = convert(testFour.input);
+    expect(result).toEqual(testFour.output);
+  });
+
+  test('testFive', () => {
+    const testFive = {
+      input: 14000000000,
+      output: 10000000000,
+    };
+    const result = convert(testFive.input);
+    expect(result).toEqual(testFive.output);
   });
 });
