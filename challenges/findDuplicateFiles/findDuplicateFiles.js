c5a42d1d893b2a1c4b58aeceeda7bbd2006d03dd
Chris McDermut <chrismcdermut@gmail.com>
2020-09-18 17:19:57 -0600

    find dup files is working, but not with super robust test

--- challenges/findDuplicateFiles/findDuplicateFiles.js
+++ challenges/findDuplicateFiles/findDuplicateFiles.js
@@ -1,5 +1,154 @@
-function findDuplicateFiles(input) {
+const fs = require('fs');
+const crypto = require('crypto');
 
+function sampleHashFile(path) {
+  const file = fs.statSync(path);
+
+  const sampleSize = 4000;
+  const totalBytes = file.size;
+
+  const hash = crypto.createHash('sha512');
+
+  // If the file is too short to take 3 samples, hash the entire file
+  if (totalBytes < sampleSize * 3) {
+    hash.update(fs.readFileSync(path));
+  } else {
+    const numBytesBetweenSamples = (totalBytes - sampleSize * 3) / 2;
+
+    const buffer = Buffer.alloc(sampleSize * 3);
+
+    // Read first, middle, and last bytes
+    for (let offsetMultiplier = 0; offsetMultiplier <= 2; offsetMultiplier++) {
+      const fd = fs.openSync(path, 'r');
+
+      const offset = offsetMultiplier * sampleSize;
+      const position = offsetMultiplier * (sampleSize + numBytesBetweenSamples);
+
+      fs.readSync(fd, buffer, offset, sampleSize, position);
+    }
+
+    hash.update(buffer);
+  }
+
+  return hash.digest();
 }
 
-module.exports = findDuplicateFiles;
+function slowFindDuplicateFiles(startingDirectory) {
+  const filesSeenAlready = {};
+  const stack = [startingDirectory];
+
+  const duplicates = [];
+
+  while (stack.length) {
+    const currentPath = stack.pop();
+    const currentFile = fs.statSync(currentPath);
+
+    // If it's a directory,
+    // put the contents in our stack
+    if (currentFile.isDirectory()) {
+      fs.readdirSync(currentPath).forEach((path) => {
+        stack.push(`${currentPath}/${path}`);
+      });
+
+      // If it's a file
+    } else {
+      // Get its contents
+      const fileContents = fs.readFileSync(currentPath);
+
+      // Get its last edited time
+      const currentLastEditedTime = currentFile.mtime;
+
+      // If we've seen it before
+      if (Object.prototype.hasOwnProperty.call(filesSeenAlready, fileContents)) {
+        const existingFile = filesSeenAlready[fileContents];
+
+        if (currentLastEditedTime > existingFile.lastEditedTime) {
+          // Current file is the dupe!
+          duplicates.push([currentPath, existingFile.path]);
+        } else {
+          // Old file is the dupe!
+          duplicates.push([existingFile.path, currentPath]);
+
+          // But also update filesSeenAlready to have the new file's info
+          filesSeenAlready[fileContents] = {
+            lastEditedTime: currentLastEditedTime,
+            path: currentPath,
+          };
+        }
+
+        // If it's a new file, throw it in filesSeenAlready
+        // and record its path and last edited time,
+        // so we can tell later if it's a dupe
+      } else {
+        filesSeenAlready[fileContents] = {
+          lastEditedTime: currentLastEditedTime,
+          path: currentPath,
+        };
+      }
+    }
+  }
+
+  return duplicates;
+}
+
+function optimizedFindDuplicateFiles(startingDirectory) {
+  const filesSeenAlready = {};
+  const stack = [startingDirectory];
+
+  const duplicates = [];
+
+  while (stack.length) {
+    const currentPath = stack.pop();
+    const currentFile = fs.statSync(currentPath);
+
+    // If it's a directory,
+    // put the contents in our stack
+    if (currentFile.isDirectory()) {
+      fs.readdirSync(currentPath).forEach((path) => {
+        stack.push(`${currentPath}/${path}`);
+      });
+
+      // If it's a file
+    } else {
+      // // Get its contents
+      // const fileContents = fs.readFileSync(currentPath);
+      // Get its hash
+      const fileHash = sampleHashFile(currentPath);
+
+      // Get its last edited time
+      const currentLastEditedTime = currentFile.mtime;
+
+      // If we've seen it before
+      if (Object.prototype.hasOwnProperty.call(filesSeenAlready, fileHash)) {
+        const existingFile = filesSeenAlready[fileHash];
+
+        if (currentLastEditedTime > existingFile.lastEditedTime) {
+          // Current file is the dupe!
+          duplicates.push([currentPath, existingFile.path]);
+        } else {
+          // Old file is the dupe!
+          duplicates.push([existingFile.path, currentPath]);
+
+          // But also update filesSeenAlready to have the new file's info
+          filesSeenAlready[fileHash] = {
+            lastEditedTime: currentLastEditedTime,
+            path: currentPath,
+          };
+        }
+
+        // If it's a new file, throw it in filesSeenAlready
+        // and record its path and last edited time,
+        // so we can tell later if it's a dupe
+      } else {
+        filesSeenAlready[fileHash] = {
+          lastEditedTime: currentLastEditedTime,
+          path: currentPath,
+        };
+      }
+    }
+  }
+
+  return duplicates;
+}
+
+module.exports = { slowFindDuplicateFiles, optimizedFindDuplicateFiles };
--- challenges/findDuplicateFiles/findDuplicateFiles.test.js
+++ challenges/findDuplicateFiles/findDuplicateFiles.test.js
@@ -1,13 +1,42 @@
-const findDuplicateFiles = require('./findDuplicateFiles');
+const { slowFindDuplicateFiles, optimizedFindDuplicateFiles } = require('./findDuplicateFiles');
 
-const testOne = {
-  input: '',
-  output: '',
-};
+describe('slowFindDuplicateFiles Test', () => {
+  // const cwd = process.cwd();
+  // console.log('cwd');
+  // console.log(cwd);
 
-describe('findDuplicateFiles Test', () => {
-  test('testOne', () => {
-    const result = findDuplicateFiles(testOne.input);
-    expect(result).toEqual(testOne.output);
+  test('testOne duplicates', () => {
+    const analyzeDuplicateFileDirectory = '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles';
+    const result = slowFindDuplicateFiles(analyzeDuplicateFileDirectory);
+    const answer = [['/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles/file2.txt',
+      '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles/file1.txt']];
+    expect(result).toEqual(answer);
+  });
+
+  test('testTwo no duplicates', () => {
+    const analyzeNoDuplicateFileDirectory = '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/noDuplicateFiles';
+    const result = slowFindDuplicateFiles(analyzeNoDuplicateFileDirectory);
+    const answer = [];
+    expect(result).toEqual(answer);
+  });
+});
+
+describe('optimizedFindDuplicateFiles Test', () => {
+  const cwd = process.cwd();
+  console.log('cwd');
+  console.log(cwd);
+  test('testOne duplicates', () => {
+    const analyzeDuplicateFileDirectory = '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles';
+    const result = optimizedFindDuplicateFiles(analyzeDuplicateFileDirectory);
+    const answer = [['/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles/file2.txt',
+      '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/duplicateFiles/file1.txt']];
+    expect(result).toEqual(answer);
+  });
+
+  test('testOne no duplicates', () => {
+    const analyzeNoDuplicateFileDirectory = '/Users/chrismcdermut/Sites/codePractice/challenges/findDuplicateFiles/noDuplicateFiles';
+    const result = optimizedFindDuplicateFiles(analyzeNoDuplicateFileDirectory);
+    const answer = [];
+    expect(result).toEqual(answer);
   });
 });
