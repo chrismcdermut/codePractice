b8d554e582d1774d4ac5af0151b47c204a73d804
Chris McDermut <chrismcdermut@gmail.com>
2020-10-01 12:22:48 -0600

    saving work for hypehn words

--- challenges/buildWordCloud/buildWordCloud.test.js
+++ challenges/buildWordCloud/buildWordCloud.test.js
@@ -25,7 +25,7 @@ describe('buildWordCloud Test', () => {
     expect(wordCloud.wordsToCounts).toEqual(testOne.output);
   });
 
-  test('testOne', () => {
+  test('testTwo', () => {
     const testOne = {
       input: 'We came, we saw, we ate cake. Friends, Romans, countrymen! Let us eat cake. New tourists in New York often wait in long lines for cronuts.',
       output: new Map([
@@ -54,5 +54,59 @@ describe('buildWordCloud Test', () => {
     };
     const wordCloud = new WordCloud(testOne.input);
     expect(wordCloud.wordsToCounts).toEqual(testOne.output);
+  });
+
+  test('testThree', () => {
+    const testOne = {
+      input: 'We came, we saw, we ate funnel-cake.',
+      output: new Map([
+        ['came', 1],
+        ['we', 3],
+        ['saw', 1],
+        ['ate', 1],
+        ['funnel-cake', 1],
+      ]),
+    };
+    const wordCloud = new WordCloud(testOne.input);
+    expect(wordCloud.wordsToCounts).toEqual(testOne.output);
+  });
+});
+
+describe('capitalize Test', () => {
+  test('testOne', () => {
+    const testOne = {
+      string: 'We came, we saw, we ate cake. Friends, Romans, countrymen! Let us eat cake. New tourists in New York often wait in long lines for cronuts.',
+      input: 'beating',
+      output: 'Beating',
+    };
+    const wordCloud = new WordCloud(testOne.string);
+    const result = wordCloud.capitalize(testOne.input);
+    expect(result).toEqual(testOne.output);
+  });
+});
+
+describe('isLetter Test', () => {
+  test('testOne', () => {
+    const testOne = {
+      string: 'After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.',
+      input: 1,
+      output: false,
+    };
+
+    const wordCloud = new WordCloud(testOne.string);
+    const result = wordCloud.isLetter(testOne.input);
+    expect(result).toEqual(testOne.output);
+  });
+
+  test('testTwo', () => {
+    const testOne = {
+      string: 'After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.',
+      input: 'a',
+      output: true,
+    };
+
+    const wordCloud = new WordCloud(testOne.string);
+    const result = wordCloud.isLetter(testOne.input);
+    expect(result).toEqual(testOne.output);
   });
 });
