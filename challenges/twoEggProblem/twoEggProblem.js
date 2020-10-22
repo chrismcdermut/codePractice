6003ce303722b5ff096e34f76418bc160be2330c
Chris McDermut <chrismcdermut@gmail.com>
2020-10-22 14:51:00 -0600

    quadratic formula use

--- challenges/twoEggProblem/twoEggProblem.js
+++ challenges/twoEggProblem/twoEggProblem.js
@@ -1,35 +1,11 @@
-// function solve(aVar, bVar, cVar) {
-//   let a = parseInt(aVar, 10);
-//   let b = parseInt(bVar, 10);
-//   let c = parseInt(cVar, 10);
-//   if (Number.isNaN(a)) { a = 1; }
-//   if (Number.isNaN(b)) { b = 0; }
-//   if (Number.isNaN(c)) { c = 0; }
-//   const D = b * b - 4 * a * c;
-//   console.log('logging D');
-//   console.log(D);
-//   return D;
-// }
-
-function solve(aVar, bVar, cVar) {
-  // let a = parseInt(aVar, 10);
-  // let b = parseInt(bVar, 10);
-  // let c = parseInt(cVar, 10);
-  // if (Number.isNaN(a)) { a = 1; }
-  // if (Number.isNaN(b)) { b = 0; }
-  // if (Number.isNaN(c)) { c = 0; }
-  // const D = b * b - 4 * a * c;
-  // console.log('logging D');
-  // console.log(D);
-  const result1 = (((-1 * bVar) + Math.sqrt(bVar ** 2) - (4 * aVar * cVar)) / (2 * aVar));
-  const result2 = (((-1 * bVar) - Math.sqrt(bVar ** 2) - (4 * aVar * cVar)) / (2 * aVar));
-  return [result1, result2];
-}
-
+const { quadSolver } = require('quadratic-solver');
 
 function twoEggProblem(numberOfFloors) {
-  // n^2+n−2*numberOfFloors=0
-  return solve(1, 1, 2 * numberOfFloors);
+  const rootsArr = quadSolver(1, 1, -2 * numberOfFloors);
+  console.log('rootsArr');
+  console.log(rootsArr);
+  const roundedNum = Math.round(rootsArr[0]);
+  return roundedNum;
 }
 
 module.exports = twoEggProblem;
