c413d08c1c9d6dcb7724bb73a5b148abde4cc15a
Chris McDermut <chrismcdermut@gmail.com>
2020-11-11 17:42:16 -0700

    saving stuff

--- challenges/smartSInt/smartSInt.js
+++ challenges/smartSInt/smartSInt.js
@@ -1,5 +1,78 @@
-function smartSInt(input) {
+function smartSInt(array1, array2) {
+  // const firstArrayMap = {};
+  const firstArrayMap = new Map();
+  array1.forEach((item) => {
+    if (!firstArrayMap[item]) {
+      // firstArrayMap[item] = 1;
+      firstArrayMap.set(item, 1);
+    } else if (firstArrayMap[item]) {
+      // firstArrayMap[item] += 1;
+      firstArrayMap.set(item, (firstArrayMap[item] += 1));
+    }
+  });
 
+  console.log('firstArrayMap');
+  console.log(firstArrayMap);
+
+  // const secondArrayMap = {};
+  const secondArrayMap = new Map();
+  array2.forEach((item) => {
+    if (!secondArrayMap[item]) {
+      // secondArrayMap[item] = 1;
+      secondArrayMap.set(item, 1);
+    } else if (secondArrayMap[item]) {
+      // secondArrayMap[item] += 1;
+      secondArrayMap.set(item, (secondArrayMap[item] += 1));
+    }
+  });
+
+  console.log('secondArrayMap');
+  console.log(secondArrayMap);
+
+  const finalSharedElementsMap = {};
+  // const firstArrayKeys = Object.keys(firstArrayMap);
+  const firstArrayKeys = firstArrayMap.keys(firstArrayMap);
+  console.log('firstArrayKeys');
+  console.log(firstArrayKeys);
+
+  // firstArrayKeys.forEach((item) => {
+  //   // if (secondArrayMap[item]) {
+  //   if (secondArrayMap.has(item)) {
+  //     finalSharedElementsMap[item] = firstArrayMap.get(item) > secondArrayMap.get(item)
+  //       ? secondArrayMap.get(item) : firstArrayMap.get(item);
+  //   }
+  // });
+
+  firstArrayKeys.forEach((key)=>{
+    console.log()
+  }
+
+
+    (item) => {
+    // if (secondArrayMap[item]) {
+    if (secondArrayMap.has(item)) {
+      finalSharedElementsMap[item] = firstArrayMap.get(item) > secondArrayMap.get(item)
+        ? secondArrayMap.get(item) : firstArrayMap.get(item);
+    }
+  });
+
+  // for (const key of firstArrayKeys) {
+  //   if (secondArrayMap.has(key)) {
+  //     finalSharedElementsMap[key] = firstArrayMap.get(key) > secondArrayMap.get(key)
+  //       ? secondArrayMap.get(key) : firstArrayMap.get(key);
+  //   }
+  // }
+
+  const finalSharedElementsMapArray = [];
+  const finalSharedElementsMapKeys = Object.keys(finalSharedElementsMap);
+
+  finalSharedElementsMapKeys.forEach((item) => {
+    for (let i = 0; i < finalSharedElementsMap[item]; i++) {
+      finalSharedElementsMapArray.push(parseInt(item, 10));
+    }
+  });
+
+  return finalSharedElementsMapArray;
 }
 
 module.exports = smartSInt;
--- challenges/smartSInt/smartSInt.test.js
+++ challenges/smartSInt/smartSInt.test.js
@@ -1,15 +1,16 @@
-const smartSInt = require('./smartSInt')
+const smartSInt = require('./smartSInt');
 
 const testOne = {
-  input: '',
-  output: ''
-}
+  array1: [1, 2, 3, 4, 5, 5, 5, 6, 7],
+  array2: [5, 5, 6, 7, 8, 9, 10],
+  output: [5, 5, 6, 7],
+};
 
 describe('smartSInt Test', () => {
-
-  test('testOne', ()=>{
-    let result = smartSInt(testOne.input)
+  test('testOne', () => {
+    const result = smartSInt(testOne.array1, testOne.array2);
+    console.log('result');
+    console.log(result);
     expect(result).toEqual(testOne.output);
   });
-
 });
