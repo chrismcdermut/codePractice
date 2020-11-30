e979efc9b4dfaadf0b72881baf6ebf5338f2a3f6
Chris McDermut <chrismcdermut@gmail.com>
2020-11-30 16:02:06 -0700

    click total is working

--- challenges/findDomainClickTotal/findDomainClickTotal.js
+++ challenges/findDomainClickTotal/findDomainClickTotal.js
@@ -1,51 +1,24 @@
 function findDomainClickTotal(countsInput) {
-  const finalCountTally = {
-
-  };
+  const finalCountTally = {};
 
   countsInput.forEach((countInfo) => {
     // first split the number
     // then split the string
     const splitInfo = countInfo.split(',');
-    const clickCount = splitInfo[0];
+    const clickCount = parseInt(splitInfo[0], 10);
     const clickedDomain = splitInfo[1];
     const splitClickedDomain = clickedDomain.split('.');
 
-    // THIS IS LESS BEAUTIFUL RIGHT HERE - PULL THIS OUT INTO COMPONENT FUNCTION
-    for (let i = splitClickedDomain.length - 1; i > 0; i--) {
-      // construct current domain
-      //      let currentDomain = splitClickedDomain[i]
-      let currentDomain = '';
-
-      // the problem is here
-      for (let j = i; j < splitClickedDomain.length; j++) {
-        const delimeter = j >= splitClickedDomain.length - 1 ? '.' : '';
-        currentDomain += delimeter + splitClickedDomain[j];
-      }
+    splitClickedDomain.forEach((item, i) => {
+      const currentDomain = splitClickedDomain.slice(i).join('.');
 
       if (!finalCountTally[currentDomain]) {
-        console.log('currentDomain');
-        console.log(currentDomain);
-        console.log('clickCount');
-        console.log(clickCount);
         finalCountTally[currentDomain] = clickCount;
-        console.log('current finalCountTally');
-        console.log(finalCountTally);
       } else {
         finalCountTally[currentDomain] += clickCount;
-        console.log('currentDomain');
-        console.log(currentDomain);
-        console.log('clickCount');
-        console.log(clickCount);
-        finalCountTally[currentDomain] = clickCount;
-        console.log('current finalCountTally');
-        console.log(finalCountTally);
       }
-    }
+    });
   });
-
-  console.log('finalCountTally');
-  console.log(finalCountTally);
   return finalCountTally;
 }
 
--- challenges/findDomainClickTotal/findDomainClickTotalNotes.md
+++ challenges/findDomainClickTotal/findDomainClickTotalNotes.md
@@ -160,3 +160,35 @@ function countsAreCorrect() {
 }
 
 countsAreCorrect()
+
+//   // THIS IS LESS BEAUTIFUL RIGHT HERE - PULL THIS OUT INTO COMPONENT FUNCTION
+//   for (let i = splitClickedDomain.length - 1; i > 0; i--) {
+//     // construct current domain
+//     //      let currentDomain = splitClickedDomain[i]
+//     let currentDomain = '';
+//
+//     // the problem is here
+//     for (let j = i; j < splitClickedDomain.length; j++) {
+//       const delimeter = j >= splitClickedDomain.length - 1 ? '.' : '';
+//       currentDomain += delimeter + splitClickedDomain[j];
+//     }
+//
+//     if (!finalCountTally[currentDomain]) {
+//       console.log('currentDomain');
+//       console.log(currentDomain);
+//       console.log('clickCount');
+//       console.log(clickCount);
+//       finalCountTally[currentDomain] = clickCount;
+//       console.log('current finalCountTally');
+//       console.log(finalCountTally);
+//     } else {
+//       finalCountTally[currentDomain] += clickCount;
+//       console.log('currentDomain');
+//       console.log(currentDomain);
+//       console.log('clickCount');
+//       console.log(clickCount);
+//       finalCountTally[currentDomain] = clickCount;
+//       console.log('current finalCountTally');
+//       console.log(finalCountTally);
+//     }
+//   }
