54e265e6e7a5b6fbccf611904cb69bb19ef46f4f
Chris McDermut <chrismcdermut@gmail.com>
2020-12-04 14:28:45 -0700

    tests working

--- challenges/flightMovies/flightMovies.js
+++ challenges/flightMovies/flightMovies.js
@@ -1,23 +1,19 @@
-// find two movies that equal flightTime
 function flightMovies(flightTime, movieList) {
   let perfectMovieTimesExist = false;
-  // start at first movie and add it to next movies until it equals
-  // flightTime wouldbebrute forceapproach,can do somethingliketthis
   const seenMovies = new Set();
+
   for (let i = 0; i < movieList.length; i++) {
     const firstMovieSeenLength = movieList[i];
     const secondMovieLength = flightTime - firstMovieSeenLength;
+
     if (seenMovies.has(secondMovieLength)) {
       perfectMovieTimesExist = true;
-      console.log('IN BLOCK');
-      console.log(perfectMovieTimesExist);
       break;
     }
-    console.log('just before adding');
+
     seenMovies.add(firstMovieSeenLength);
   }
-  console.log('before return');
-  console.log(perfectMovieTimesExist);
+
   return perfectMovieTimesExist; /* boolean */
 }
 
