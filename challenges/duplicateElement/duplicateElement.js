6b21e6fa038ce48bc02526e2837777df33aaa592
Chris McDermut <chrismcdermut@gmail.com>
2020-12-04 15:56:30 -0700

    saving work

--- challenges/duplicateElement/duplicateElement.js
+++ challenges/duplicateElement/duplicateElement.js
@@ -1,10 +1,32 @@
-function duplicateElementCheck(input) {
+function duplicateElementCheck(listArg) {
+  const copiedList = listArg;
+  const sortedList = copiedList.sort();
   let duplicateElement;
-  // sort array
+  let min = 0; /* index */
+  let max = sortedList.length; /* index */
+  let middle; /* index */
+  let counter = 0;
+
   // divide in two, if the last element is less then firstelement plus n the duplicate is in there.
-  for (let i = 0; i < input.length; i++) {
-    console.log(i);
+  while (min <= max && counter < 100) {
+    middle = Math.floor((min + max) / 2);
+    console.log('middle');
+    console.log(middle);
+    // have a break out/return
+
+    // if last value is < than beginnging + n (length of list), then dup is in that half
+    if (sortedList[max - 1] < (sortedList[middle] + (max - 1 - middle))) {
+      console.log('IN THIS GOOFY IF STATEMENT');
+      console.log('sortedList[middle]');
+      console.log(sortedList[middle]);
+      min = middle;
+    } else {
+      max = middle;
+    }
+
+    counter += 1;
   }
+
   return duplicateElement;
 }
 
--- challenges/duplicateElement/duplicateElement.test.js
+++ challenges/duplicateElement/duplicateElement.test.js
@@ -1,22 +1,20 @@
 const duplicateElementCheck = require('./duplicateElement');
 
-const testOne = {
-  input: [1, 2, 3, 4, 5, 6, 7, 8, 1, 9, 10],
-  output: 1,
-};
-
-const testOneA = {
-  input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10],
-  output: 1,
-};
-
 describe('duplicateElementCheck Test', () => {
   test('testOne', () => {
+    const testOne = {
+      input: [1, 2, 3, 4, 5, 6, 7, 8, 1, 9, 10],
+      output: 1,
+    };
     const result = duplicateElementCheck(testOne.input);
     expect(result).toEqual(testOne.output);
   });
 
   test('testOneA', () => {
+    const testOneA = {
+      input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10],
+      output: 9,
+    };
     const result = duplicateElementCheck(testOneA.input);
     expect(result).toEqual(testOneA.output);
   });
