0f2708c644f60998b8ff310f623da357249139ab
Chris McDermut <chrismcdermut@gmail.com>
2020-12-08 14:52:38 -0700

    tested debounceD

--- challenges/debounce/debounce.js
+++ challenges/debounce/debounce.js
@@ -3,11 +3,21 @@ function debounce(fn, time) {
 
   return function callFunction() {
     const functionCall = () => fn.apply(this);
-
     clearTimeout(timeout);
     timeout = setTimeout(functionCall, time);
   };
 }
+
+function debounce2(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+module.exports = { debounce, debounce2 };
 
 module.exports.runDebounce = function runDebounce() {
   debounce();
--- challenges/debounce/debounce.test.js
+++ challenges/debounce/debounce.test.js
@@ -1,9 +1,45 @@
-const runDebounce = require('./debounce');
+const { debounce, debounce2 } = require('./debounce');
+// tell jest to mock all timeout functions
+jest.useFakeTimers();
 
-describe('runDebounce Test', () => {
-  xtest('testOne WILL FAIL', () => {
-    expect(() => {
-      runDebounce();
-    }).toEqual();
+describe('debounce', () => {
+  let func = jest.Mock;
+  let debouncedFunc = Function;
+
+  beforeEach(() => {
+    func = jest.fn();
+    debouncedFunc = debounce(func, 1000);
+  });
+
+  test('execute just once', () => {
+    for (let i = 0; i < 100; i++) {
+      debouncedFunc();
+    }
+
+    // fast-forward time
+    jest.runAllTimers();
+
+    expect(func).toBeCalledTimes(1);
+  });
+});
+
+describe('debounce2', () => {
+  let func = jest.Mock;
+  let debouncedFunc = Function;
+
+  beforeEach(() => {
+    func = jest.fn();
+    debouncedFunc = debounce2(func, 1000);
+  });
+
+  test('execute just once', () => {
+    for (let i = 0; i < 100; i++) {
+      debouncedFunc();
+    }
+
+    // fast-forward time
+    jest.runAllTimers();
+
+    expect(func).toBeCalledTimes(1);
   });
 });
